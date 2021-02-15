10e5c9341db7fc6f5de98f39f4d6555d9994fa04
Akash <kothariakash58@gmail.com>
2021-02-15 22:17:28 +0530

    first commit

---
+++ music/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+
+class Album(models.Model):
+    artist=models.CharField(max_length=250)
+    album_title=models.CharField(max_length=500)
+    genre=models.CharField(max_length=100)
+    album_logo=models.CharField(max_length=1000)
+
+    def get_absolute_url(self):
+        return reverse ('music:detail' , kwargs={'pk':self.pk})
+
+    def __str__(self):
+        return self.album_title + '-' + self.artist
+
+
+
+#As song are related to particular album
+#cascade logic if album delete that particular song also delete
+class Song(models.Model):
+    album=models.ForeignKey(Album, on_delete=models.CASCADE)
+    file_type=models.CharField(max_length=100)
+    song_title=models.CharField(max_length=250)
+    is_favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.song_title
---
+++ music/urls.py
@@ -0,0 +1,24 @@
+from . import  views
+from django.conf.urls import url
+
+app_name='music'
+
+urlpatterns = [
+    #/music/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+
+    # /misic/<album_id>
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    #/music/album/add
+    url(r'album/add/$' , views.AlbumCreate.as_view(), name='album-add'),
+
+    #/music/album/2/
+    url(r'album/(?P<pk>[0-9]+)/$' , views.AlbumUpdate.as_view(), name='album-update'),
+
+    #/music/album/2/delete/
+    url(r'album/(?P<pk>[0-9]+)/delete$' , views.AlbumDelete.as_view(), name='album-delete'),
+
+    
+]
